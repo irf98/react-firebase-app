e6f95ad2b8893700a5e0ebe51b0ccd11fb680ef0
irf98 <ignacioramosfarinho@gmail.com>
2021-09-01 11:41:00 -0300

    segunda entrega

---
+++ src/components/NavBar.js
@@ -0,0 +1,26 @@
+import './NavBar.css';
+
+const NavBar = () => {
+    return (
+        <nav className='NavBar'>
+            <div className='NavLogo'>
+                <h1 className='LogoName'>Web Store</h1>
+            </div>
+            <div className='LeftNav'>
+                <div className='NavOptionsLeft'>
+                    <button className='Option'>Home</button>
+                    <button className='Option'>About</button>
+                    <button className='Option'>Wishlist</button>
+                </div>
+            </div>
+            <div className='RightNav'>
+                <div className='NavOptionsRight'>
+                    <button className='Option'>Sign In</button>
+                    <button className='Option'>Sign Up</button>
+                </div>
+            </div>
+        </nav>
+    );
+}
+
+export default NavBar;
